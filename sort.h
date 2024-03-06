ecad4bd42cb8a1103214c7a60ea22b4fcf8ea264
Junior Mabunda <mjmabunda0@gmail.com>
2024-03-06 09:38:14 +0200

    [FIX] Betty coding style

--- sort.h
+++ sort.h
@@ -10,9 +10,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Prints a list of integers */
@@ -21,12 +21,14 @@ void print_list(const listint_t *list);
 /* Prints an array of integers */
 void print_array(const int *array, size_t size);
 
-/* Sorts an array of integers in ascending order
+/*
+ * Sorts an array of integers in ascending order
  * using the bubble sort algorithm
  */
 void bubble_sort(int *array, size_t size);
 
-/* Sorts a doubly linked list of integers in ascending
+/*
+ * Sorts a doubly linked list of integers in ascending
  * order using the insertion sort algorithm
  */
 void insertion_sort_list(listint_t **list);
@@ -34,17 +36,20 @@ void insertion_sort_list(listint_t **list);
 /* Swaps two adjacent nodes of a doubly linked list */
 void swap_list(listint_t **list, listint_t *head, int ix);
 
-/* Sorts a doubly linked list of integers in ascending
+/*
+ * Sorts a doubly linked list of integers in ascending
  * order using the selection sort algorithm
  */
 void selection_sort(int *array, size_t size);
 
-/* Sorts an array of integers in ascending
+/*
+ * Sorts an array of integers in ascending
  * order using the quick sort algorithm
  */
 void quick_sort(int *array, size_t size);
 
-/* Helper function the the quick_sort function,
+/*
+ * Helper function the the quick_sort function,
  * handles the sorting
  */
 void quick_sort_helper(int *array, size_t size, int low, int high);
